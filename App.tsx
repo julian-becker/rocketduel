450464a3694fbba92cf0658e2bade14e96609ac4
Nayan Hajratwala <nayan@chikli.com>
2020-05-12 16:58:36 -0400

    amazing UI work

--- App.tsx
+++ App.tsx
@@ -1,19 +1,109 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to Rocket Duel</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Welcome to Rocket Duel</Text>
+            </View>
+            <View style={styles.main}>
+                <Opponent/>
+                <Settings/>
+            </View>
+            <View style={styles.actions}>
+                <FireButton/>
+            </View>
+        </View>
+    );
 }
 
+export function Opponent() {
+    return (
+        <View>
+            <Text style={styles.label}>Opponent</Text>
+            <Text>{coordinate()}°, {coordinate()}°</Text>
+        </View>
+    )
+}
+
+function coordinate() {
+    return Math.trunc(Math.random() * 10000000) / 100000
+}
+
+function fire() {
+}
+
+export function Settings() {
+    return (
+        <View>
+            <Thrust/>
+            <Angle/>
+        </View>
+    )
+}
+
+export function Thrust() {
+    return (
+        <View>
+            <Text style={styles.label}>Thrust</Text>
+            <TextInput style={styles.dataEntry}>999</TextInput><Text>N</Text>
+        </View>
+    )
+}
+
+export function Angle() {
+    return (
+        <View>
+            <Text style={styles.label}>Angle</Text>
+            <CoordinateEntry axis="X"/>
+            <CoordinateEntry axis="Y"/>
+            <CoordinateEntry axis="Z"/>
+        </View>
+    )
+}
+
+interface CoordinateEntryParams {
+    axis: String;
+}
+
+export function CoordinateEntry(props: CoordinateEntryParams) {
+    return (
+        <View>
+            <TextInput style={styles.dataEntry}>100</TextInput>
+            <Text>{props.axis}</Text>
+        </View>
+    )
+}
+
+function FireButton() {
+    return <Button title={"Fire!!!"} onPress={fire}/>;
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: 75,
+        paddingLeft: 25
+    },
+    header: {
+        flex: 1
+    },
+    title: {
+        fontSize: 25
+    },
+    main: {
+        flex: 5
+    },
+    actions: {
+        flex: 1
+    },
+    label: {
+        fontWeight: "bold"
+    },
+    dataEntry: {
+        fontWeight: "100"
+    }
 });
