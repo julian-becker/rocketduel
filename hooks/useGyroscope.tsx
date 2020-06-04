6ce930d6751c207227328634456b2ef3b29e0f69
Jim Wrubel <jwrubel@gmail.com>
2020-06-03 20:02:32 -0400

    fix incorrect Android elevation value. Connects #53.

--- hooks/useGyroscope.tsx
+++ hooks/useGyroscope.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Platform } from 'react-native';
 import { isEmulatorSync } from 'react-native-device-info';
 import { Accelerometer } from 'expo-sensors';
 
@@ -8,14 +9,21 @@ const useGyroscope = (dispatch: Function) => {
     const getGyroscope = async () => {
       const isGyroscopeAvailable = await Accelerometer.isAvailableAsync();
       if (isGyroscopeAvailable && !isEmulatorSync()) {
-        Accelerometer.setUpdateInterval(200);
+        Accelerometer.setUpdateInterval(1000);
         const _subscription = Accelerometer.addListener(data => {
-          // data in the form of { x, y, z } - We only care about y
-          const { y } = data;
+          // iOS and Android report these values differently
+          // iOS reports pitch as the y value, in degrees
           // when y is -1 that corresponds to a 90 degree elevation
           // when y is 0, that's 0 elevation.
           // fwiw, y of 1 is when the phone is facing directly down
-          const elevation = - 180 * (y / 2);
+          // Android makes you work for it. 
+
+          // data in the form of { x, y, z }
+          const { x, y, z } = data;
+
+          const androidPitch = (Math.atan2(z, -y) * 180 / Math.PI) - 90; // convert to degrees
+          const iOSPitch = - 180 * (y / 2);
+          const elevation = Platform.OS === 'ios' ? iOSPitch : androidPitch ;
           dispatch({type: 'UPDATE_ELEVATION', value: Math.round(elevation)});
         });
       }
