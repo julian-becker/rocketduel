a06f4b974c9a7c845f2384b3628bed1d13ef47a9
Jim Wrubel <jwrubel@gmail.com>
2020-06-09 14:08:04 -0400

    try a different slider control for thrust. Connects #58.

--- components/ThrustSlider.tsx
+++ components/ThrustSlider.tsx
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { BodyText } from './styled/Text';
-import Slider from '@react-native-community/slider';
+import VerticalSlider from 'rn-vertical-slider';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
 import { PlayerContext } from '../contexts/Player';
 import { ProjectileContext } from '../contexts/Projectile';
+import { silver } from '../components/styled/Colors';
 
 const ThrustSlider = () => {
 
@@ -13,26 +14,42 @@ const ThrustSlider = () => {
   const { projectile, dispatchProjectile } = useContext(ProjectileContext);
   const { thrust } = projectile;
 
-  const setThrust = async (thrust) => {
+  const handleThrustChange = async () => {
     const options = {
       enableVibrateFallback: false,
       ignoreAndroidSystemSettings: false
     };
     ReactNativeHapticFeedback.trigger('selection', options);
+  }
+
+  const setThrust = (thrust) => {
     dispatchProjectile({type: 'UPDATE_THRUST', value: thrust});
   }
 
   return (
     <View style={styles.thrust}>
-      <BodyText>{`Azimuth: ${azimuth} Elevation: ${elevation}`}</BodyText>
-      <Slider
-        style={styles.thrustSlider}
-        minimumValue={0}
-        maximumValue={100}
-        step={1}
-        value={thrust}
-        onValueChange={setThrust}
-      />
+      <BodyText>{`Azimuth: ${azimuth}°`}</BodyText>
+      <BodyText>{`Elevation: ${elevation}°`}</BodyText>
+      <View style={styles.sliderWrap}>
+        <VerticalSlider
+          width={16}
+          height={200}
+          showBackgroundShadow={true}
+          min={0}
+          max={100}
+          step={1}
+          value={thrust}
+          borderRadius={1}
+          minimumTrackTintColor={silver.metallic}
+          maximumTrackTintColor={silver.darkGunmetal}
+          onChange={(value: number) => {
+            handleThrustChange()
+          }}
+          onComplete={(value: number) => {
+            setThrust(value);
+          }}
+        />
+      </View>
       <BodyText>Thrust: {thrust}</BodyText>
     </View>
   )
@@ -42,7 +59,10 @@ const styles = StyleSheet.create({
   thrust: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'flex-start'
+    justifyContent: 'space-between'
+  },
+  sliderWrap: {
+    margin: 5
   },
   thrustSlider: {
     width: '100%',
