4dd74c46bdbef44d14ebce131368ec8a52b60379
Jim Wrubel <jwrubel@gmail.com>
2020-06-10 07:08:27 -0400

    remove dropdown since we don't need it to report status. Connects #58.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import DropdownAlert from 'react-native-dropdownalert';
 
 import HomeScreen from './screens/Home';
 import Setup from './screens/Setup';
 import GameWrapper from './screens/GameWrapper';
 import YouWin from './screens/YouWin';
-import { blue, green, red } from './components/styled/Colors';
-
-import { DropDownHolder } from './components/DropDownHolder';
 
 
 const Stack = createStackNavigator();
@@ -24,14 +20,6 @@ const App = () => {
         <Stack.Screen name="Game" component={GameWrapper} />
         <Stack.Screen name="YouWin" component={YouWin} options={{ title: 'You Win!' }}/>
       </Stack.Navigator>
-      <DropdownAlert 
-        ref={(ref) => DropDownHolder.setDropDown(ref)}
-        closeInterval={4000}
-        onClose={(data) => DropDownHolder.invokeOnClose(data)}
-        successColor={green.forest}
-        infoColor={blue}
-        errorColor={red}
-      />
     </NavigationContainer>
   );
 };
