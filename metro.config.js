b450b62e202c72b2c2a7d669ae8993a4ec943d3b
Jim Wrubel <jwrubel@gmail.com>
2020-06-17 11:33:33 -0400

    add rn-really-awesome-button and svg support. Connects #66.

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,17 @@
-module.exports = {
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+  return {
     transformer: {
-        assetPlugins: ['expo-asset/tools/hashAssetFiles']
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      assetPlugins: ['expo-asset/tools/hashAssetFiles']
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
     }
-};
+  };
+})();
