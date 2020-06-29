753c96d21689870353145fca0242835975f6e768
Jim Wrubel <jwrubel@gmail.com>
2020-06-29 09:29:27 -0400

    add 3d assets and allow metro bundler to use them. Connects #72.

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,18 @@
 const { getDefaultConfig } = require("metro-config");
 
 module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts }
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer"),
-      assetPlugins: ['expo-asset/tools/hashAssetFiles']
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"]
-    }
-  };
-})();
+    const {
+      resolver: { sourceExts, assetExts }
+    } = await getDefaultConfig();
+    assetExts.push("model", "obj", "mtl", "gltf", "glb", "dae"); // for 3d models - add unusual extensions here
+    return {
+      transformer: {
+        babelTransformerPath: require.resolve("react-native-svg-transformer"),
+        assetPlugins: ['expo-asset/tools/hashAssetFiles']
+      },
+      resolver: {
+        assetExts: assetExts.filter(ext => ext !== "svg"),
+        sourceExts: [...sourceExts, "svg"]
+      }
+    };
+  })();
