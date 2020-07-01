0adeb7e0bf405cc1fccb49d4bdfe2228fd979709
Jim Wrubel <jwrubel@gmail.com>
2020-07-01 09:03:49 -0400

    import all robot types and support dynamic typing. Connects #59.

--- components/enemies/RobotIcon.tsx
+++ components/enemies/RobotIcon.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import Robot from '../../assets/robots/redbot.svg';
+
+import Bluebot from '../../assets/robots/bluebot.svg';
+import Pinkbot from '../../assets/robots/pinkbot.svg';
+import Purplebot from '../../assets/robots/purplebot.svg';
+import Redbot from '../../assets/robots/redbot.svg';
+import Silverbot from '../../assets/robots/silverbot.svg';
+import Yellowbot from '../../assets/robots/yellowbot.svg';
+
 const RobotIcon = (props) => {
 
   const height = props.height || 24;
-  const RATIO = 2 / 3; // robot design scale, width / height
-  return ( 
-    <Robot height={height} width={height * RATIO} />
-  )
+  const robotType = props.type || 'red'
+  switch (robotType) {
+    case 'blue':
+      return <Bluebot height={height} />;
+    case 'pink':
+      return <Pinkbot height={height} />;
+    case 'purple':
+      return <Purplebot height={height} />;
+    case 'red':
+      return <Redbot height={height} />;
+    case 'silver':
+      return <Silverbot height={height} />;
+    case 'yellow':
+      return <Yellowbot height={height} />;
+    default:
+      return <Redbot height={height} />;
+  }
 };
 
 export default RobotIcon;
